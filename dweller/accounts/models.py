c23aad18449e45ab31395b7dad584d6a7bc0c0fc
kawrou <121672581+kawrou@users.noreply.github.com>
2025-02-27 18:04:21 +0000

    feat:Create custom User model manager

--- dweller/accounts/models.py
+++ dweller/accounts/models.py
@@ -1,8 +1,42 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.utils import timezone
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
+class MyUserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_active", True) 
+        extra_fields.setdefault("is_superuser", False) 
+        extra_fields.setdefault("is_staff", False) 
+        return self._create_user(email, password, **extra_fields)
+
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff = True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser = True.")
+
+        return self._create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
     username = None
@@ -11,6 +45,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name", "date_of_birth"]
+    
+    objects = MyUserManager()
 
     def __str__(self):
         return self.email
