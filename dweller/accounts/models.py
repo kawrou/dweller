35898c78481e3f5894392bee3deacb69b286a550
kawrou <121672581+kawrou@users.noreply.github.com>
2025-02-27 16:32:53 +0000

    feat:Create custom User including date_of_birth

--- dweller/accounts/models.py
+++ dweller/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 # Create your models here.
+
+class User(AbstractUser):
+    username = None
+    date_of_birth = models.DateField()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "date_of_birth"]
+
+    def __str__(self):
+        return self.email
--- dweller/dwellr/settings.py
+++ dweller/dwellr/settings.py
@@ -84,6 +84,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "accounts.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
