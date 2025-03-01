0f6750ab90b27b4f408bbf0ec5c61284ea5ee5b2
kawrou <121672581+kawrou@users.noreply.github.com>
2025-03-01 00:10:18 +0000

    feat:Validate users age for UserCreationForm

--- dweller/accounts/forms.py
+++ dweller/accounts/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 from .models import User as UserModel
+import datetime
+from django.utils.translation import gettext as _
 
 User: type[UserModel] = get_user_model()
 
@@ -10,6 +12,18 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ["email", "first_name", "last_name", "date_of_birth", "password1", "password2"]
+
+    def clean_date_of_birth(self):
+        dob = self.cleaned_data["date_of_birth"]
+
+        today = datetime.date.today() 
+        age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+        if age < 18:
+            raise ValidationError(
+                _("You must be at least 18 years old to register."),
+            )
+
+        return dob
 
     def save(self, commit = True) -> UserModel:
         user: UserModel = super().save(commit=False)
