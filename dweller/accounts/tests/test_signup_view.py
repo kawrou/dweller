77502d8f57d19eca154dd08ac497ae9febfc6fa1
kawrou <121672581+kawrou@users.noreply.github.com>
2025-03-01 22:36:45 +0000

    test:Add sign up fixtures to conftest.py for shared test data

--- dweller/accounts/tests/test_signup_view.py
+++ dweller/accounts/tests/test_signup_view.py
@@ -1,24 +1,5 @@
 import pytest
 from django.urls import reverse
-from django.test import Client
-
-valid_form_data = {
-    'email': "normal@user.com",
-    'first_name': "John",
-    'last_name': "Doe",
-    'date_of_birth': "1990-01-01",
-    'password1': "StrongPassword123!",
-    'password2': "StrongPassword123!",
-}
-
-invalid_password_not_matched_form_data = {
-    'email': "normal@user.com",
-    'first_name': "John",
-    'last_name': "Doe",
-    'date_of_birth': "1990-01-01",
-    'password1': "StrongPassword123!",
-    'password2': "StrongPassword456!",
-}
 
 @pytest.mark.django_db
 def test_signup_page_loads(client):
@@ -33,14 +14,14 @@ def test_signup_view_uses_correct_template(client):
 
 @pytest.mark.skip
 @pytest.mark.django_db
-def test_valid_signup_redirect(client):
+def test_valid_signup_redirect(client, valid_form_data):
     response = client.post(reverse("signup"), valid_form_data)
     assert response.status_code == 200
     assert response.url == reverse("login")
 
 @pytest.mark.skip
 @pytest.mark.django_db
-def test_invalid_signup_shows_errors(client):
+def test_invalid_signup_shows_errors(client, invalid_password_not_matched_form_data):
     response = client.post(reverse("signup"), invalid_password_not_matched_form_data)
     assert response.status_code == 200
 
