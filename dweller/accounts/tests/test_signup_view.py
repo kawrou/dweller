1214edfdc4fc56e1bc46a83fd07380f1abfb7a11
kawrou <121672581+kawrou@users.noreply.github.com>
2025-03-01 22:48:20 +0000

    feat:wrote signup.html using UserCreationForm

--- dweller/accounts/tests/test_signup_view.py
+++ dweller/accounts/tests/test_signup_view.py
@@ -6,11 +6,10 @@ def test_signup_page_loads(client):
     response = client.get(reverse("signup"))
     assert response.status_code == 200 
 
-@pytest.mark.skip
 @pytest.mark.django_db
 def test_signup_view_uses_correct_template(client):
     response = client.get(reverse("signup"))
-    assert "signup.html" in [t.name for t in response.templates]
+    assert "accounts/signup.html" in [t.name for t in response.templates]
 
 @pytest.mark.skip
 @pytest.mark.django_db
@@ -19,7 +18,6 @@ def test_valid_signup_redirect(client, valid_form_data):
     assert response.status_code == 200
     assert response.url == reverse("login")
 
-@pytest.mark.skip
 @pytest.mark.django_db
 def test_invalid_signup_shows_errors(client, invalid_password_not_matched_form_data):
     response = client.post(reverse("signup"), invalid_password_not_matched_form_data)
