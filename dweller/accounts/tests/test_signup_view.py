ec0f6c89a6595b31c846e95cb60945d234be7e80
kawrou <121672581+kawrou@users.noreply.github.com>
2025-03-02 01:40:58 +0000

    test:Update test case for view redirect to homepage on success and non matching password error

--- dweller/accounts/tests/test_signup_view.py
+++ dweller/accounts/tests/test_signup_view.py
@@ -11,16 +11,18 @@ def test_signup_view_uses_correct_template(client):
     response = client.get(reverse("signup"))
     assert "accounts/signup.html" in [t.name for t in response.templates]
 
-@pytest.mark.skip
 @pytest.mark.django_db
 def test_valid_signup_redirect(client, valid_form_data):
     response = client.post(reverse("signup"), valid_form_data)
-    assert response.status_code == 200
-    assert response.url == reverse("login")
+    assert response.status_code == 302
+    assert response.url == reverse("index")
 
 @pytest.mark.django_db
 def test_invalid_signup_shows_errors(client, invalid_password_not_matched_form_data):
     response = client.post(reverse("signup"), invalid_password_not_matched_form_data)
     assert response.status_code == 200
-
-
+    assert "form" in response.context
+    
+    form = response.context["form"]
+    assert not form.is_valid()
+    assert "password2" in form.errors
