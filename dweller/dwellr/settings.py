f6aee40749eac57d9273a7deb11cc4254316bcf5
kawrou <121672581+kawrou@users.noreply.github.com>
2025-03-06 10:45:51 +0000

    feat:Base Templates with nav bar and dynamic buttons for authentication state

--- dweller/dwellr/settings.py
+++ dweller/dwellr/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'dwellr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
