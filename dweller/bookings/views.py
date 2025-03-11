6154c189a2792d2a0055741249893fb86108a5c7
kawrou <121672581+kawrou@users.noreply.github.com>
2025-03-11 18:27:52 +0000

    feat:Use Booking model in view and html template

--- dweller/bookings/views.py
+++ dweller/bookings/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpRequest
+from bookings.models import Booking
 
 # Create your views here.
 @login_required
 def trips_view(request: HttpRequest):
-    return render(request, 'bookings/trips.html')
+    user_bookings = Booking.objects.filter(user=request.user)
+    return render(request, 'bookings/trips.html', {"bookings": user_bookings})
