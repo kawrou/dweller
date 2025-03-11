0868728b09715065a6c2e72b4d126f942a6bfdd1
kawrou <121672581+kawrou@users.noreply.github.com>
2025-03-11 18:44:50 +0000

    feat:Add __str__ method to Booking model

--- dweller/bookings/models.py
+++ dweller/bookings/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 
 # Create your models here.
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     destination = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Booking by {self.user.email} to {self.destination}"
