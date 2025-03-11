0298ede29adefd004c7b32dbbf5515bca579f87e
kawrou <121672581+kawrou@users.noreply.github.com>
2025-03-11 16:09:00 +0000

    feat:Login required for trips page

--- dweller/bookings/views.py
+++ dweller/bookings/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpRequest
 
 # Create your views here.
+@login_required
 def trips_view(request: HttpRequest):
     return render(request, 'bookings/trips.html')
