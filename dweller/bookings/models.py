a5d8f27e5b8000e9d2be83433e4150f4847aa4eb
kawrou <121672581+kawrou@users.noreply.github.com>
2025-03-11 18:07:46 +0000

    feat:Create basic bookings model

--- dweller/bookings/models.py
+++ dweller/bookings/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+from dweller.accounts.models import User
+
 # Create your models here.
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    destination = models.CharField(max_length=255)
