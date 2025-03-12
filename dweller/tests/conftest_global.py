9080534b4986285ac5ba219fbc3032ffabe76b2e
kawrou <121672581+kawrou@users.noreply.github.com>
2025-03-12 23:23:17 +0000

    test:Add user and booking fixtures to global conftest

--- dweller/tests/conftest_global.py
+++ dweller/tests/conftest_global.py
@@ -1,11 +1,39 @@
 import pytest
+from django.contrib.auth import get_user_model
+from bookings.models import Booking
+
+CustomUser = get_user_model()
 
 @pytest.fixture
-def normal_user():  
-    return {
-        'email': "normal@user.com",
-        'password': "foo",
-        'first_name': "John",
-        'last_name': "Doe",
-        'dob': "1990-01-01"
-    }
+def test_user1():
+    return CustomUser.objects.create_user(
+        email="user1@user.com",
+        password="StrongPassword123",
+        first_name="John",
+        last_name="Doe",
+        date_of_birth="1990-01-01",
+    )
+
+@pytest.fixture
+def test_user2():
+    return CustomUser.objects.create_user(
+        email="user2@user.com",
+        password="StrongPassword456",
+        first_name="John",
+        last_name="Smith",
+        date_of_birth="1990-01-01",
+    )
+
+@pytest.fixture
+def test_booking1(test_user1):
+    return Booking.objects.create(
+        user=test_user1,
+        destination="Paris"
+    )
+
+@pytest.fixture
+def test_booking2(test_user2):
+    return Booking.objects.create(
+        user=test_user2,
+        destination="London"
+    )
