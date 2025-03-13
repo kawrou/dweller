9c773226146579fdd8b42d600634e59a6ad243cc
kawrou <121672581+kawrou@users.noreply.github.com>
2025-03-13 16:32:51 +0000

    feat:Add fieldset to booking admin page

--- dweller/accounts/admin.py
+++ dweller/accounts/admin.py
@@ -2,32 +2,53 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from accounts.models import User
 
+
 # As guided by the documentation when using a custom User model
 # https://docs.djangoproject.com/en/5.1/topics/auth/customizing/#a-full-example
 class UserAdmin(BaseUserAdmin):
-    list_display = ["email", "first_name", "last_name", "date_of_birth", "is_staff", "is_active"]
+    list_display = [
+        "email",
+        "first_name",
+        "last_name",
+        "date_of_birth",
+        "is_staff",
+        "is_active",
+    ]
 
     fieldsets = [
         (None, {"fields": ["email", "password"]}),
         ("Personal info", {"fields": ["first_name", "last_name", "date_of_birth"]}),
-        ("Permissions", {
-            "fields": [
-                "is_admin", 
-                "is_staff", 
-                "is_active", 
-                "is_superuser", 
-                "groups", 
-                "user_permissions"
+        (
+            "Permissions",
+            {
+                "fields": [
+                    "is_admin",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
                 ]
-            }),
-        ("Important Dates", {"fields": ["last_login", "date_joined"]})
+            },
+        ),
+        ("Important Dates", {"fields": ["last_login", "date_joined"]}),
     ]
 
     add_fieldsets = [
-        (None, {
-            "classes": ["wide"],
-            "fields": ["email", "first_name", "last_name", "date_of_birth", "password1", "password2"],
-        })
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": [
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "date_of_birth",
+                    "password1",
+                    "password2",
+                ],
+            },
+        )
     ]
 
     search_fields = ["email"]
--- dweller/bookings/admin.py
+++ dweller/bookings/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Booking
+
+
 # Register your models here.
-admin.site.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ["user", "destination"]
+    fieldsets = [(None, {"fields": ["user", "destination"]})]
+
+    search_fields = ["user"]
+    ordering = ["user"]
+    filter_horizontal = []
+
+
+admin.site.register(Booking, BookingAdmin)
