ede296b18b5d7666538598ee95f1403e4563bdf9
kawrou <121672581+kawrou@users.noreply.github.com>
2025-03-13 00:26:31 +0000

    fix:home view test

--- dweller/core/tests/test_views.py
+++ dweller/core/tests/test_views.py
@@ -5,10 +5,7 @@ from django.urls import reverse
 # Create your tests here.
 @pytest.mark.django_db
 def test_home_view_returns_hello_world(client):
-    url = reverse('index')
+    url = reverse('home')
     response = client.get(url)
 
     assert response.status_code == 200
-
-    content = response.content.decode('utf-8')
-    assert "Hello World" in content
