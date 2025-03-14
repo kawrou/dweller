504893c112668a7505aa2c8f8cf5a7ce714a5c46
kawrou <121672581+kawrou@users.noreply.github.com>
2025-03-14 13:07:52 +0000

    feat:installed dotenv to allow settings.py to use env variables

--- dweller/dwellr/settings.py
+++ dweller/dwellr/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -94,9 +95,11 @@ WSGI_APPLICATION = 'dwellr.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dweller_db',
-        'HOST': '127.0.0.1',
-        'POST': '5432'
+        'NAME': os.getenv('POSTGRES_DB', 'dweller_db'),
+        'USER': os.getenv('POSTGRES_USER', ''),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('POSTGRES_HOST', '127.0.0.1'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432')
     }
 }
 
